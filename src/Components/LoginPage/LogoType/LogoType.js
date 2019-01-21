ab4e5af25e9c07ac80b0b4105fa13a23c6687585
Stavros Tsirlidis <stavros.tsirlidis@student.kyh.se>
2019-01-21 15:57:09 +0100

    Added Logotyp folder and .js file, changed some styling

---
+++ src/Components/LoginPage/LogoType/LogoType.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Styled from 'styled-components';
+import LogotypeImg from"./logotype.png";
+
+const Logotype = Styled.div`
+   
+    height:auto;
+    margin-top:10px;
+    background-color:SaddleBrown;
+
+`;
+
+
+const logotype = () =>( 
+    <Logotype>
+       <img src={LogotypeImg }/>
+    </Logotype>
+)
+
+export default logotype ;
