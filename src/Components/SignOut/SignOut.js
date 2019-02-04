8ab90440ebc98a7282c11cd5fadeaba7db255a78
Stavros Tsirlidis <stivve@hotmail.com>
2019-02-04 09:53:31 +0100

    changed the signout button

--- src/Components/SignOut/SignOut.js
+++ src/Components/SignOut/SignOut.js
@@ -1,10 +1,11 @@
 import { withFirebase } from '../Firebase'
 import React from 'react';
+import {Button} from './SignOutStyle';
 
 const SingOutButton = ({Firebase}) => (
-    <button type="button" onClick={Firebase.doSignOut}>
-     Sign Out
-    </button>
+    <Button type="button" onClick={Firebase.doSignOut}>
+    <i data-tip="Sign out" class="fas fa-sign-out-alt"></i>
+    </Button>
 );
 
 export default withFirebase(SingOutButton);
---
+++ src/Components/SignOut/SignOutStyle.js
@@ -0,0 +1,13 @@
+import Styled from 'styled-components';
+
+export const Button = Styled.button `
+color:white;
+background:transparent;
+font-size: 36px;
+border:none;
+display: inline-block;
+cursor:pointer;
+&:focus{
+   outline: none;
+}
+`; 
